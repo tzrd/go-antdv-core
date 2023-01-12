9b37765c4b7bbe3ab49346860f0af75e85e7290b
leon zhang <leon@leondeMacBook-Pro.local>
2023-01-12 21:20:54 +0800

    Support custom DriverDigitFunc

--- captcha/captcha.go
+++ captcha/captcha.go
@@ -22,10 +22,10 @@ func SetStore(s base64Captcha.Store) {
 	base64Captcha.DefaultMemStore = s
 }
 
-func DriverDigitFunc() (id, b64s string, err error) {
+func DriverDigitFunc(height int, width int, length int, maxSkew float64, dotCount int) (id, b64s string, err error) {
 	e := configJsonBody{}
 	e.Id = uuid.New().String()
-	e.DriverDigit = base64Captcha.NewDriverDigit(80, 240, 4, 0.7, 80)
+	e.DriverDigit = base64Captcha.NewDriverDigit(height, width, length, maxSkew, dotCount)
 	driver := e.DriverDigit
 	cap := base64Captcha.NewCaptcha(driver, base64Captcha.DefaultMemStore)
 	return cap.Generate()
