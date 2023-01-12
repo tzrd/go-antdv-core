ae34c701fd95425369933b387d86ac4561b77353
leon zhang <leon@leondeMacBook-Pro.local>
2023-01-12 20:53:30 +0800

    first commit

---
+++ captcha/captcha.go
@@ -0,0 +1,37 @@
+package captcha
+
+import (
+	"github.com/google/uuid"
+	"github.com/mojocn/base64Captcha"
+)
+
+// configJsonBody json request body.
+type configJsonBody struct {
+	Id            string
+	CaptchaType   string
+	VerifyValue   string
+	DriverAudio   *base64Captcha.DriverAudio
+	DriverString  *base64Captcha.DriverString
+	DriverChinese *base64Captcha.DriverChinese
+	DriverMath    *base64Captcha.DriverMath
+	DriverDigit   *base64Captcha.DriverDigit
+}
+
+// SetStore 设置store
+func SetStore(s base64Captcha.Store) {
+	base64Captcha.DefaultMemStore = s
+}
+
+func DriverDigitFunc() (id, b64s string, err error) {
+	e := configJsonBody{}
+	e.Id = uuid.New().String()
+	e.DriverDigit = base64Captcha.NewDriverDigit(80, 240, 4, 0.7, 80)
+	driver := e.DriverDigit
+	cap := base64Captcha.NewCaptcha(driver, base64Captcha.DefaultMemStore)
+	return cap.Generate()
+}
+
+// Verify 校验验证码
+func Verify(id, code string, clear bool) bool {
+	return base64Captcha.DefaultMemStore.Verify(id, code, clear)
+}
